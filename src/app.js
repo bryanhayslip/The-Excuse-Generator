1612afad5590b22909015efaf41b3e9afe4c2d64
bryanhayslip <bryan.hayslip@gmail.com>
2022-04-10 20:19:59 +0000

    Completed project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let who = [
+    "my sister",
+    "his Dad",
+    "their priest",
+    "my neighbor",
+    "the mayor"
+  ];
+  let action = ["punched", "kissed", "kicked", "slapped", "ate"];
+  let what = ["my car", "her house", "his diary", "some cheese", "Guy Fieri"];
+  let when = [
+    "last night",
+    "this morning",
+    "a few moments ago",
+    "yesterday",
+    "on Tuesday evening"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * action.length);
+  let whenIndex = Math.floor(Math.random() * action.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
